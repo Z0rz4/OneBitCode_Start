044dcbfd9d242f3886c0bc58b50c6d76db44fb59
Kalel Zorzanello Soares <118759989+Z0rz4@users.noreply.github.com>
2025-03-06 12:43:51 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,23 @@
 function calculaTipo(event){
     event.preventDefault();
     let conta = document.getElementById('conta').value;
-    let QualidadeServ = document.getElementById('QualidadeServ').value;
-    let NumeroDePessoas = document.getElementById('pessoas').value;
+    let qualidadeServ = document.getElementById('QualidadeServ').value;
+    let numeroDePessoas = document.getElementById('pessoas').value;
 
-    if(conta == '' | QualidadeServ == 0){
+    if(conta == '' | qualidadeServ == 0){
         alert("Por favor, preencha os valores");
         return;
     }
 
-    if(NumeroDePessoas == "" | pessoas <= 1){
-        NumeroDePessoas = 1;
+    if(numeroDePessoas == "" | pessoas <= 1){
+        numeroDePessoas = 1;
         document.getElementById('each').style.display = "none";
     } else {
         document.getElementById('each').style.display = "block";
 
     }
 
-    let total = (conta * QualidadeServ) / NumeroDePessoas;
+    let total = (conta * qualidadeServ) / numeroDePessoas;
     total = total.toFixed(2);
     document.getElementById('tip').innerHTML = total;
     document.getElementById('totalTip').style.display = "block";
